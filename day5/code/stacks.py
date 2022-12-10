504547dde704f6a89c0a28155cbc07fca1d21b7e
Carter4242 <92061520+Carter4242@users.noreply.github.com>
2022-12-10 03:19:48 -0500

    Day 4 done semi-ok, but data specific rows-cols

---
+++ day5/code/stacks.py
@@ -0,0 +1,33 @@
+class stacks():
+
+    def stacks():
+        data = []
+        with open('day5/input/sample.txt', 'r') as file:
+            data = file.readlines()
+        for i in range(len(data)):
+            data[i] = data[i].strip('\n')
+        stacks = []
+        for i in range(1, 37, 4):
+            stacks.append([])
+        stackNum = 0
+        for i in range(7, -1, -1):
+            for e in range(1, 37, 4):
+                if data[i][e] != ' ':
+                    stacks[stackNum].append(data[i][e])
+                stackNum += 1
+            stackNum = 0
+        for i in range(10, len(data)):
+            before = data[i].split(" from")[0]
+            after = data[i].split(" from")[1]
+            numCrates = int(before[5:])
+            fromStack = int(after[1])-1
+            toStack = int(after[6])-1
+            for i in range(numCrates):
+                stacks[toStack].append(stacks[fromStack].pop())
+        output = ""
+        for stack in stacks:
+            output += stack[-1]
+        print(output)
+            
+
+    stacks()
