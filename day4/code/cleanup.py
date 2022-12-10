319327413ff2ceb14ce54bd4f94f5bacf4edaba5
Carter4242 <92061520+Carter4242@users.noreply.github.com>
2022-12-10 02:35:13 -0500

    Day done, p2 was a 6 character change

---
+++ day4/code/cleanup.py
@@ -0,0 +1,24 @@
+class cleanup():
+
+    def cleanup():
+        data = []
+        with open('day4/input/sample.txt', 'r') as file:
+            data = file.readlines()
+        for i in range(len(data)):
+            data[i] = data[i].strip().split(',')
+        total = 0
+        for item in data:
+            one = int(item[0].split('-')[0])
+            two = int(item[0].split('-')[1])
+            three = int(item[1].split('-')[0])
+            four = int(item[1].split('-')[1])
+            if (three <= one <= four and three <= two <= four) or (one <= three <= two and one <= four <= two):
+                # print(one, two, three, four)
+                total += 1
+        print(total)
+
+
+            
+
+        
+    cleanup()
