a463222f0a680d4a8c5181a4dfb8d47c1b37d44d
Carter4242 <92061520+Carter4242@users.noreply.github.com>
2022-12-11 01:10:54 -0500

    Day 10 part 2 done, good one!

--- day9/code/bridges2.py
+++ day9/code/bridges2.py
@@ -7,36 +7,39 @@ class bridges():
         for i in range(len(data)):
             data[i] = data[i].strip()
         setVisited = set()
-        head = [0,0]
-        tail = [0,0]
-        setVisited.add('F' + str(tail[0]) + 'S' + str(tail[1]))
+        knots = []
+        for i in range(10):
+            knots.append([0,0])
+        setVisited.add('F' + str(knots[9][0]) + 'S' + str(knots[9][1]))
         for i in data:
             command = i.split()[0]
             move = int(i.split()[1])
             for i in range(move):
                 match command:
                     case "R":
-                        head[0] += 1
+                        knots[0][0] += 1
                     case "L":
-                        head[0] -= 1
+                        knots[0][0] -= 1
                     case "U":
-                        head[1] += 1
+                        knots[0][1] += 1
                     case "D":
-                        head[1] -= 1
-                deltaX = head[0] - tail[0]
-                deltaY = head[1] - tail[1]
-                if abs(deltaX) <= 1 and abs(deltaY) <= 1:
-                    continue
-                if abs(deltaX) < abs(deltaY):
-                    tail[0] = head[0]
-                    tail[1] = head[1] - deltaY // abs(deltaY)
-                elif abs(deltaX) > abs(deltaY):
-                    tail[0] = head[0] - deltaX // abs(deltaX)
-                    tail[1] = head[1]
-                else:
-                    tail[0] = head[0] - deltaX // abs(deltaX)
-                    tail[1] = head[1] - deltaY // abs(deltaY)
-                setVisited.add('F' + str(tail[0]) + 'S' + str(tail[1]))
+                        knots[0][1] -= 1
+                for e in range(1, 10):
+                    deltaX = knots[e-1][0] - knots[e][0]
+                    deltaY = knots[e-1][1] - knots[e][1]
+                    if abs(deltaX) <= 1 and abs(deltaY) <= 1:
+                        continue
+                    if abs(deltaX) < abs(deltaY):
+                        knots[e][0] = knots[e-1][0]
+                        knots[e][1] = knots[e-1][1] - deltaY // abs(deltaY)
+                    elif abs(deltaX) > abs(deltaY):
+                        knots[e][0] = knots[e-1][0] - deltaX // abs(deltaX)
+                        knots[e][1] = knots[e-1][1]
+                    else:
+                        knots[e][0] = knots[e-1][0] - deltaX // abs(deltaX)
+                        knots[e][1] = knots[e-1][1] - deltaY // abs(deltaY)
+                    if e == 9:
+                        setVisited.add('F' + str(knots[e][0]) + 'S' + str(knots[e][1]))
         print(len(setVisited))
 
 
