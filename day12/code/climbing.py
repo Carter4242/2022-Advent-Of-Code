c8cac6bc56e2e1df7ac06692aab9de57caa07d2b
Carter4242 <92061520+Carter4242@users.noreply.github.com>
2022-12-12 03:16:55 -0500

    Day 12 done, try-except is how to stop indexErrors

---
+++ day12/code/climbing.py
@@ -0,0 +1,70 @@
+class climbing():
+
+    def climbing():
+        data = []
+        with open('day12/input/sample.txt', 'r') as file:
+            data = file.readlines()
+        for i in range(len(data)):
+            data[i] = data[i].strip()     
+
+        grid = []
+
+        for i in range(len(data)):
+            grid.append([])
+            for c in data[i]:
+                appended = ord(c)-ord('a')
+                if appended != -14 and appended != -28:
+                    grid[i].append(appended)
+                elif appended == -28:
+                    grid[i].append(25) 
+                else:
+                    grid[i].append(0) 
+                
+        for g in grid:
+            print(g)
+        
+        steps = 0
+        visited = set(tuple([20,0]))
+        next = []
+        current = [[20,0]]
+        while (True):
+            for i in current:
+                spot = grid[i[0]][i[1]] # int
+                if i[1]-1 != -1:
+                    left = grid[i[0]][i[1]-1]
+                    if spot+1 >= left:
+                        if tuple([i[0],i[1]-1]) not in visited:
+                            visited.add(tuple([i[0],i[1]-1]))
+                            next.append([i[0],i[1]-1])
+                try:
+                    right = grid[i[0]][i[1]+1]
+                    if spot+1 >= right:
+                        if tuple([i[0],i[1]+1]) not in visited:
+                            visited.add(tuple([i[0],i[1]+1]))
+                            next.append([i[0],i[1]+1])
+                except:
+                    pass
+                if i[0]-1 != -1:
+                    top = grid[i[0]-1][i[1]]
+                    if spot+1 >= top:
+                        if tuple([i[0]-1,i[1]]) not in visited:
+                            visited.add(tuple([i[0]-1,i[1]]))
+                            next.append([i[0]-1,i[1]])
+                try:
+                    bottom = grid[i[0]+1][i[1]]
+                    if spot+1 >= bottom:
+                        if tuple([i[0]+1,i[1]]) not in visited:
+                            visited.add(tuple([i[0]+1,i[1]]))
+                            next.append([i[0]+1,i[1]])
+                except:
+                    pass
+            steps += 1
+            if tuple([20,36]) in visited:
+                break
+            print(steps)
+            current = next.copy()
+            next = []
+        print(steps)
+            
+
+    climbing()
